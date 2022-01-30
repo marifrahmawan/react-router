2c303f45e7c505e6a1fd5edc8716bda2553bb1a7
marifrahmawan <marifrahmawan@gmail.com>
2022-01-30 21:31:45 +0800

    switch and exact

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Products = () => {
   return (
     <section>
       <h1>Products Page</h1>
       <ul>
-        <li>Book</li>
-        <li>Ball</li>
-        <li>Shoes</li>
+        <li>
+          <Link to="/products/p1">Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Ball</Link>
+        </li>
+        <li>
+          <Link to="/products/p3"> Shoes</Link>
+        </li>
       </ul>
     </section>
   );
